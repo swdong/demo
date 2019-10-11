91439f5685cc3007bda317e447d996d9f3dc94cb
swdong <939611388@qq.com>
2019-10-11 18:36:21 +0800

    代码3.0

---
+++ Hello_Git/src/hello/Hello.java
@@ -0,0 +1,18 @@
+package hello;
+
+/**
+ * @deprecated 上传到GIt的测试代码3.0
+ * @author Administrator
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git , I come from IDEA.");
+        System.out.println("Welcome! Nice to meet you.");
+        System.out.println();
+        /**
+         * there is no problem and Error.
+         */
+        Integer i = new Integer(1);
+        System.out.println(12345);
+    }
+}
